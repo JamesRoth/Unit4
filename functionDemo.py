6b9a6cac6053be1f81b736322200d19439a5e934
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-09 08:26:07 -0500

    Updated from Brython Server: 3/9/2018 8:26:07 AM

--- functionDemo.py
+++ functionDemo.py
@@ -17,9 +17,14 @@ double("wowwow")
 double(True)
 """
 
+"""
 def bigger(a,b):
     if a>b:
         print(a)
     else:
        print(b)
 bigger(12, 5)
+"""
+
+def slope(x1,y1,x2,y2):
+    print((y1-y2)/(x1-x2))
