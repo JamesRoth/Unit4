fc2617e6337c79015ab317566d0905efc7b74b44
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-19 08:40:54 -0400

    Updated from Brython Server: 3/19/2018 8:40:53 AM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -1,1 +1,31 @@
+#James Roth
+#3/19/18
+#colorChangeWindow.py - function that changes a window's color
 
+from random import randint
+from ggame import *
+
+color=Color(0x00ff00,1)
+color=Color(0xff0000,1)
+color=Color(0x0000ff,1)
+color=Color(0x00ffff,1)
+color=Color(0xff00ff,1)
+color=Color(0xffffff,1)
+
+def mouseClick(event):
+    int=randint(1,6)
+    if int==1:
+        color=Color(0x00ff00,1)
+    elif int==2:
+        color=Color(0xff0000,1)
+    elif int==3:
+        color=Color(0x0000ff,1)
+    elif int==4:
+        color=Color(0x00ffff,1)
+    elif int==5:
+        color=Color(0xff00ff,1)
+    else:
+        color=Color(0xffffff,1)
+
+App().listenMouseEvent("click", mouseClick)
+App.run()
