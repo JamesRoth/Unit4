8c68b0c76bb0207f777042bb6e3879af1097aeb0
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-19 08:04:48 -0400

    Update functionDemo.py

--- functionDemo.py
+++ functionDemo.py
@@ -24,14 +24,16 @@ def bigger(a,b):
     else:
        print(b)
 bigger(12, 5)
-"""
 
 def slope(x1,y1,x2,y2):
     print((y1-y2)/(x1-x2))
 #slope(10,5,3,4)
 i=1 #surprisingly enough, if you input 1 for true, the loop will run as true, since python outputs the True boolean as 1
-while 1:
+while 1: #not a good habit
     print("James")
     i+=1
     if i==10:
         break
+"""
+
+print("The max of 3 and 4 is:", max(3,4))
