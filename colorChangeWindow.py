eeff85ebaeee165e760a75696be91af62f156697
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-19 08:46:54 -0400

    Updated from Brython Server: 3/19/2018 8:46:53 AM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -10,16 +10,16 @@ outline=LineStyle(0xffffff,1)
 color=Color(0xffff00,1)
 
 def mouseClick(event):
-    int=randint(1,6)
-    if int==1:
+    num=randint(1,6)
+    if num==1:
         color=Color(0x00ff00,1)
-    elif int==2:
+    elif num==2:
         color=Color(0xff0000,1)
-    elif int==3:
+    elif num==3:
         color=Color(0x0000ff,1)
-    elif int==4:
+    elif num==4:
         color=Color(0x00ffff,1)
-    elif int==5:
+    elif num==5:
         color=Color(0xff00ff,1)
     else:
         color=Color(0xffffff,1)
