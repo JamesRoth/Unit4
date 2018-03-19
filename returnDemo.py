0b8e4c22a3720b414ea6fe09a35cf4385d634de3
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-19 08:09:07 -0400

    Updated from Brython Server: 3/19/2018 8:09:06 AM

--- returnDemo.py
+++ returnDemo.py
@@ -1,1 +1,12 @@
+#James Roth
+#3/19/18
+#returnDemo.py - how to use return
 
+from random import randint
+
+def even(low,high):
+    int=randint(low,high)
+    if int%2==0:
+        print(int)
+    else:
+        
