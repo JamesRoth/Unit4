d0e6c3a41463afb5059a5fd2222da63e257cfc4e
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-26 08:33:21 -0400

    Updated from Brython Server: 3/26/2018 8:33:21 AM

--- printSquares.py
+++ printSquares.py
@@ -2,12 +2,12 @@
 #3/26/18
 #printSquares - function that prints squares 
 
-def squares(r,c):
-    print("+--+--"*(c-1) + "+")
-    while r>1: 
-        print("|  |  "*(c-1) + "|")
-        print("+--+--"*(c-1) + "+")
-        r-=1
+def squares(c,r):
+    print("+--+--"*(r-1) + "+")
+    while c>1: 
+        print("|  |  "*(r-1) + "|")
+        print("+--+--"*(r-1) + "+")
+        c-=1
     print("|  |  " + "|")
     print("+--+--" + "+")
 squares(2,4)
