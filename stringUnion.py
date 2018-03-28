df8a940675e1bad7080f2be636993a6efcdc7be1
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-28 08:33:58 -0400

    Updated from Brython Server: 3/28/2018 8:33:57 AM

--- stringUnion.py
+++ stringUnion.py
@@ -1,1 +1,15 @@
+#James Roth
+#3/28/18
+#stringUion.py - joining letters
 
+def stringUnion(w1,w2):
+    string=""
+    for ch in w1:
+        if ch not in string:
+            string+=ch
+    for ch in w2:
+        if ch not in string:
+            string+=ch
+    
+
+stringUnion(Mississippi, Pennsylvania)
