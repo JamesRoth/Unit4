543f9e50f4b54e0c3af9d253a71a59e72a559b9e
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-28 08:17:06 -0400

    Updated from Brython Server: 3/28/2018 8:17:05 AM

--- distance.py
+++ distance.py
@@ -3,6 +3,6 @@
 #distance.py - distance formula function
 
 def distance(x1, y1, x2, y2):
-    return(((y1-y1)**2 + (x1-x2)**2)**1/2)
+    return(((y1-y1)**2 + (x1-x2)**2)**(1/2))
 
-distance(3,4,5,2)
+print(distance(3,4,5,2))
