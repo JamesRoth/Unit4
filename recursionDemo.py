01aad70ca7b91a6b17ab025990b01a4c24a0bb2a
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-29 08:08:43 -0400

    Updated from Brython Server: 3/29/2018 8:08:43 AM

--- recursionDemo.py
+++ recursionDemo.py
@@ -1,13 +1,13 @@
 #James Roth
 #3/29/18
-#recursionDemo.py - recursive version of countdown
+#recursionDemo.py - recursive version of countdown - a function that calls itself
 
 def countdown(n):
-    if n==0: # called the "base case"
+    if n==0: #called the "base case"
         print("BOOM!")
     else: 
         print(n)
         n-=1
         countdown(n)
 
-countdown(0)
+countdown(1000)
