15a50a932520597b56d2afda31ba04a0bb0015d0
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-29 08:18:07 -0400

    Updated from Brython Server: 3/29/2018 8:18:07 AM

--- warmup12.py
+++ warmup12.py
@@ -2,14 +2,17 @@
 #3/29/18
 #warmup12.py - returning GCF of 2 numbers
 
-def GCF(num1,num2):
+def gcf(num1,num2):
     ans=""
     MAXI=max(num1,num2)
     
     counter=min(num1,num2)
     
     while ans=="":
-        if MAXI%counter=0:
+        if MAXI%counter==0:
             ans=int(counter)
+            print(int(counter))
         else: 
             counter-=1
+
+gcf(10,5)
