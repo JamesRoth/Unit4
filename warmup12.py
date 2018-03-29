d4d6cb8d1b9042acffee638237cefcfc35bcf3e9
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-29 08:14:59 -0400

    Updated from Brython Server: 3/29/2018 8:14:59 AM

--- warmup12.py
+++ warmup12.py
@@ -1,1 +1,14 @@
+#James Roth
+#3/29/18
+#warmup12.py - returning GCF of 2 numbers
 
+def GCF(num1,num2):
+    ans=""
+    mini=min(num1,num2)
+    maxi=max(num1,num2)
+    while ans=="":
+        if maxi%mini=0:
+            ans=int(mini)
+        else: 
+            
+        
