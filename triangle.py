a5c01a6c0af30bb8dd470b20834097a609e28295
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-30 08:27:28 -0400

    Updated from Brython Server: 3/30/2018 8:27:27 AM

--- triangle.py
+++ triangle.py
@@ -3,13 +3,13 @@
 #triangle.py - verticies to area of a triangle
 
 def distance(x1, y1, x2, y2):
-    return(((y1-y2)**2 + (x1-x2)**2)**(1/2))
+    return(((y1-y2)**2 + (x1-x2)**2)**(0.5))
 
 def area(x1, y1, x2, y2, x3, y3):
     side1=distance(x1, y1, x2, y2)
     side2=distance(x2, y2, x3, y3)
     side3=distance(x3, y3, x1, y1)
     s=1/2*(side1+side2+side3)
-    return (s(s-side1)(s-side2)(s-side3))**1/2
+    return s(s-side1)(s-side2)(s-side3))**0.5
     
-print(distance(3,4,-5, 2, -7, 1))
+print(area(3, 4,-5, 2, -7, 1))
