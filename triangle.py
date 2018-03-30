9ae64e7375bcaeaa7dbaa55ab10f2de6b349a94f
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-30 08:29:46 -0400

    Updated from Brython Server: 3/30/2018 8:29:46 AM

--- triangle.py
+++ triangle.py
@@ -10,6 +10,6 @@ def area(x1, y1, x2, y2, x3, y3):
     side2=distance(x2, y2, x3, y3)
     side3=distance(x3, y3, x1, y1)
     s=(side1+side2+side3)/2
-    return (s(s-side1)*(s-side2)*(s-side3))**0.5
+    return (s*(s-side1)*(s-side2)*(s-side3))**0.5
     
 print(area(3, 4,-5, 2, -7, 1))
