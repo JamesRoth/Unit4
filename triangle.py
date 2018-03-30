82176783afe405a0c6994a8a41536203c56011f0
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-30 08:14:49 -0400

    Updated from Brython Server: 3/30/2018 8:14:49 AM

--- triangle.py
+++ triangle.py
@@ -1,1 +1,9 @@
+#James Roth
+#3/29/18
+#triangle.py - verticies to area of a triangle
 
+def area(x1, y2, x2, y2, x3, y3):
+    side1=distance(x1, y1, x2, y2)
+    side2=distance(x2, y2, x3, y3)
+    side3=distance(x3, y3, x1, y1)
+    
